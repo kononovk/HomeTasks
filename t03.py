516d35e1c79a3708ed768695d1807c13ccef6410
Nickolay <kononov.nikolay.nk1@gmail.com>
2018-09-22 23:29:24 +0300

    21
    132

--- t03.py
+++
@@ -1,3 +0,0 @@
-a = int(input("Input your number: "))
-ans = [i for i in range(1, a + 1) if a % i == 0]
-print(ans)
--- t05.py
+++
@@ -1,5 +0,0 @@
-s = input("Input string: ").lower().replace(' ', '')
-if s == s[::-1]:
-    print("This string is palindrome")
-else:
-    print("This string is not palindrome")
