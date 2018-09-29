71f9e5fbcea9a2deaaef02b7965ed9c0b2e827ec
Nickolay <kononov.nikolay.nk1@gmail.com>
2018-09-29 19:22:17 +0300

    Mistakes fixed

--- t03.py
+++ t03.py
@@ -1,4 +1,4 @@
 a = int(input("Input your number: "))
-ans = [i for i in range(-abs(a + 1), abs(a + 1)) if i != 0 and a % i == 0]
+ans = [i for i in range(-abs(a), abs(a) + 1) if i != 0 and a % i == 0]
 for i in ans:
     print(i, end=' ')
--- t04.py
+++ t04.py
@@ -1,6 +1,6 @@
 a = int(input("Input your number: "))
-ans = [i for i in range(-abs(a + 1), abs(a + 1)) if i != 0 and a % i == 0]
+ans = [i for i in range(-abs(a), abs(a) + 1) if i != 0 and a % i == 0]
 if len(ans) == 4:
-    print("THis number is prime")
+    print("This number is prime")
 else:
     print("This number is not prime")
