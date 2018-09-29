1417fd252026bf15a2ef4ca26d807529e9d748b5
Kononov Nikolay <kononov.nikolay.nk1@gmail.com>
2018-09-29 16:12:38 +0300

    update t03

--- t03.py
+++ t03.py
@@ -1,13 +1,4 @@
 a = int(input("Input your number: "))
-ans = ['±' + str(i) for i in range(1, abs(a + 1)) if a % i == 0]
-for i in ans:
-    print(i, end=' ')
-
-
-# The same code but with repeating divisors which annoy me
-"""
-a = int(input("Input your number: "))
 ans = [i for i in range(-abs(a + 1), abs(a + 1)) if i != 0 and a % i == 0]
 for i in ans:
     print(i, end=' ')
-"""
