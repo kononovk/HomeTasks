3a872876be29a694f39d782dfcf5dd54a4851273
Nickolay <kononov.nikolay.nk1@gmail.com>
2018-09-29 19:16:20 +0300

    Bugs fixed

--- t01.py
+++ t01.py
@@ -1,9 +1,11 @@
 def rotate(inp_list):
+    if len(inp_list) == 0:
+        return inp_list
     temp_list = inp_list.copy()
     temp_list.insert(0, temp_list.pop(len(temp_list) - 1))
     return temp_list
 
 
-a = [1, 2, 3]
+a = [1]
 rot_a = rotate(a)
 print("a = {}, rotated a = {}".format(a, rot_a))
