da146d68faf29c597f21644043cb57518de5f8c0
Nickolay <kononov.nikolay.nk1@gmail.com>
2018-10-06 22:02:42 +0300

    Division on directories added

--- t03.py
+++
@@ -1,8 +0,0 @@
-def num_of_dividers(inp_num):
-    dividers = [i for i in range(-abs(inp_num), abs(inp_num) + 1) if i != 0 and inp_num % i == 0]
-    return dividers
-
-
-if __name__ == '__main__':
-    num = int(input("Input your number: "))
-    print(num_of_dividers(num))
--- t04.py
+++
@@ -1,8 +0,0 @@
-from t03 import num_of_dividers
-
-inp_num = int(input("Input your number: "))
-
-if len(num_of_dividers(inp_num)) == 4:
-    print("This number is prime")
-else:
-    print("This number is not prime")
