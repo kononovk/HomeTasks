c66074875d215070d01608040406ee13e16c440a
Nickolay <kononov.nikolay.nk1@gmail.com>
2018-10-12 00:09:09 +0300

    Task number 2

---
+++ HW2/t02.py
@@ -0,0 +1,27 @@
+from unittest import TestCase, main
+
+
+def is_perfect_number(inp_num):
+    if type(inp_num) != int or inp_num < 1:
+        return "ValueError"
+
+    sum_of_dividers = 0
+    for i in range(1, inp_num):
+        if inp_num % i == 0:
+            sum_of_dividers += i
+    if sum_of_dividers == inp_num:
+        return True
+    return False
+
+
+class Validator(TestCase):
+    def test_correct_values(self):
+        self.assertEqual(is_perfect_number(1), False)
+        self.assertEqual(is_perfect_number(33550336), True)
+        self.assertEqual(is_perfect_number(28), True)
+        self.assertEqual(is_perfect_number([6]), "ValueError")
+        self.assertEqual(is_perfect_number(-28), "ValueError")
+
+
+main()
+
