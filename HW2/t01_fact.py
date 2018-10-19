9d04bb6aaa14ec940ff2f40da4058e13d0c0cf16
Nickolay <kononov.nikolay.nk1@gmail.com>
2018-10-19 19:27:54 +0300

    Factorial fixed

--- HW2/t01_fact.py
+++ HW2/t01_fact.py
@@ -5,7 +5,7 @@ from termcolor import colored
 from math import factorial
 
 
-#  Recursion factorial function #
+# Recursion factorial function #
 def fact_rec(n):
     if type(n) != int or n < 0:
         raise ValueError("n must be natural")
@@ -40,99 +40,83 @@ def cycle_fact(n):
 
 #  Test processing main class  #
 class Validator(TestCase):
-    def test_correct_values(self):
-        if not self.assertRaises(ValueError, fact, "10"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertRaises(ValueError, fact, "abc"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertRaises(ValueError, fact, 10.0):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(fact(30000), factorial(30000)):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(fact(0), 1):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertRaises(ValueError, fact, -4):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertRaises(ValueError, fact, 11.75):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertRaises(ValueError, fact, [3]):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertRaises(ValueError, fact_rec, "10"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertRaises(ValueError, fact_rec, "abc"):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertRaises(ValueError, fact_rec, 10.0):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(fact_rec(30000), factorial(30000)):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertEqual(fact_rec(0), 1):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertRaises(ValueError, fact_rec, -4):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertRaises(ValueError, fact_rec, 11.75):
-            print("Test passed")
-        else:
-            print("Test failed")
-        if not self.assertRaises(ValueError, fact_rec, [3]):
-            print("Test passed")
-        else:
-            print("Test failed")
+    def test_cycle_string1(self):
+        self.assertRaises(ValueError, fact, "10")
+
+    def test_cycle_string2(self):
+        self.assertRaises(ValueError, fact, "abc")
+
+    def test_cycle_float1(self):
+        self.assertRaises(ValueError, fact, 10.0)
+
+    def test_cycle_float2(self):
+        self.assertRaises(ValueError, fact, 11.75)
+
+    def test_cycle_num1(self):
+        self.assertEqual(fact(30000), factorial(30000))
+
+    def test_cycle_num2(self):
+        self.assertEqual(fact(0), 1)
+
+    def test_cycle_negative(self):
+        self.assertRaises(ValueError, fact, -4)
+
+    def test_cycle_list(self):
+        self.assertRaises(ValueError, fact, [3])
+
+    def test_rec_string1(self):
+        self.assertRaises(ValueError, fact_rec, "10")
+
+    def test_rec_string2(self):
+        self.assertRaises(ValueError, fact_rec, "abc")
+
+    def test_rec_float1(self):
+        self.assertRaises(ValueError, fact_rec, 10.0)
+
+    def test_rec_float2(self):
+        self.assertRaises(ValueError, fact_rec, 11.75)
+
+    def test_rec_num1(self):
+        self.assertEqual(fact_rec(30000), factorial(30000))
+
+    def test_rec_num2(self):
+        self.assertEqual(fact_rec(0), 1)
+
+    def test_rec_negative(self):
+        self.assertRaises(ValueError, fact_rec, -4)
+
+    def test_rec_list(self):
+        self.assertRaises(ValueError, fact_rec, [3])
 
 
 #  Time and memory measuring for different variable values  #
-fact_of = 10000
+fact_of = 50000
 int_time = time.process_time()
 recursion_fact(fact_of)
-print(colored("Recursion factorial function time for {} = {}".format(fact_of, time.process_time() - int_time), 'yellow'))
+print(colored("Recursion factorial function time for {} = {}".format(fact_of, time.process_time() - int_time),
+              'yellow'))
 int_time = time.process_time()
 cycle_fact(fact_of)
 print(colored("Cycle factorial function time for {} = {}".format(fact_of, time.process_time() - int_time), 'yellow'))
 print(colored("____________________________________________________________________________________________", 'yellow'))
-fact_of = 20000
+fact_of = 80000
 int_time = time.process_time()
 recursion_fact(fact_of)
-print(colored("Recursion factorial function time for {} = {}".format(fact_of, time.process_time() - int_time), 'yellow'))
+print(colored("Recursion factorial function time for {} = {}".format(fact_of, time.process_time() - int_time),
+              'yellow'))
 int_time = time.process_time()
 cycle_fact(fact_of)
 print(colored("Cycle factorial function time for {} = {}".format(fact_of, time.process_time() - int_time), 'yellow'))
 print(colored("____________________________________________________________________________________________", 'yellow'))
-fact_of = 30000
+fact_of = 110000
 int_time = time.process_time()
 recursion_fact(fact_of)
-print(colored("Recursion factorial function time for {} = {}".format(fact_of, time.process_time() - int_time), 'yellow'))
+print(colored("Recursion factorial function time for {} = {}".format(fact_of, time.process_time() - int_time),
+              'yellow'))
 int_time = time.process_time()
 cycle_fact(fact_of)
 print(colored("Cycle factorial function time for {} = {}".format(fact_of, time.process_time() - int_time), 'yellow'))
 print(colored("____________________________________________________________________________________________", 'yellow'))
 
 '# test processing #'
-print(colored("TESTS:", 'green'))
 main()
