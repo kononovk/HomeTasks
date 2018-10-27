6a2291ed3d955c1be2eaada832f608b28ebdef9d
Nickolay <kononov.nikolay.nk1@gmail.com>
2018-10-27 16:11:40 +0300

    Some bugs fixed

--- HW3/t02CsrCiph/caesar.py
+++ HW3/t02CsrCiph/caesar.py
@@ -4,7 +4,11 @@ from termcolor import colored
 if __name__ == '__main__':
     cipher_flag = input("Do you want encrypt or decrypt your message?\nPress(e/d): ")
     user_text = input("Input text: ")
-    user_offset = int(input("Input offset: "))
+
+    user_offset = int(input("Input your offset: "))
+    if user_offset not in range(-26, 26):
+        print("Your offset must be in [-25;25]")
+        user_offset = int(input("Try again: "))
 
     if cipher_flag == 'e':
         print("The encryption of your text is " + colored(encrypt(user_offset, user_text), 'yellow', attrs=['bold']))
